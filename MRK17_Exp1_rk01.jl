dca5e6eb7bf92f6763c0fd7c835267da81bfb967
Reinhold Kliegl <reinhold.kliegl@gmail.com>
2019-12-21 15:52:40 +0100

    Including some Julia-based transformations. (Note: They do not yet match the ones used in the article.)

--- MRK17_Exp1_rk01.jl
+++ MRK17_Exp1_rk01.jl
@@ -1,9 +1,17 @@
 cd(joinpath(homedir(),"Documents/ZiF_CG_WS2/MRK17_Exp1/"))
 pwd()
-using DataFramesMeta, DataFrames, InteractiveUtils, LinearAlgebra, MixedModels, RData, RCall, CSV
+using DataFramesMeta, DataFrames, InteractiveUtils, LinearAlgebra, MixedModels, RData, RCall, CSV, ShiftedArrays
 
 # Read Urdaten
 dat0 = CSV.read("MRK17_Exp1.csv")
+describe(dat0)
+
+# These transformations illustrate some Julia functionality, but they do not match what is needed.
+dat1 = @linq dat0 |>
+    transform(lagQlty = lag(:Qlty), lagTrgt = lag(:Freq)) |>
+    where(:Freq .≠ "NW", :Score .== "C", 30 .≤ :rt .≤ 3000) |>
+    select(:Subj, :Item, :trial, :Qlty, :Freq, :TPR, :lagQlty, :lagTrgt, :rt)
+describe(dat1)
 
 # Does not work
 #dat = load("MRK17_Exp1_xtra.RData") |>
@@ -11,9 +19,13 @@ dat0 = CSV.read("MRK17_Exp1.csv")
 
 # Works
 dat = CSV.read("MRK17_Exp1_xtra.csv")
+describe(dat)
 
 @time m1 = fit(LinearMixedModel,
       @formula(rrt ~ 1 + F*T*Q*lQ*lT + (1+T+Q+lQ+lT | Item) + (1+F+T+Q+lQ+lT | Subj)), dat)
+
+
+
 
 show(m1)
 m1.optsum
