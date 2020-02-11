e787d45eb575b59681baeeba0a3970d6c07c0afc
Reinhold Kliegl <reinhold.kliegl@gmail.com>
2020-02-11 12:01:48 +0100

    expand w/ fulldummy problem

--- rePCA_problem.jl
+++ rePCA_problem.jl
@@ -1,24 +1,38 @@
 # Problem: rePCA() results not invariant with linear transformation of predictor
 # Reinhold Kliegl, 2020-02-10
 
-using RData, DataFrames, DataFramesMeta, MixedModels, StatsBase
+using  DataFrames, DataFramesMeta, MixedModels, StatsBase, Feather
 
-sleepstudy = deepcopy(dat[:sleepstudy])
-colnames = ["Reaction", "Days", "Subject"]
-names!(sleepstudy, Symbol.(colnames))
+#sleepstudy = deepcopy(dat[:sleepstudy])
+#colnames = ["reaction", "days", "subj"]
+#names!(sleepstudy, Symbol.(colnames))
+
+sleepstudy =  Feather.read(joinpath(MixedModels.TestData, "sleepstudy.feather"))
+
 sleepstudy = @linq sleepstudy |>
-             transform(Days2 = :Days .*2,
-                       Days3 = :Days ./2)
+             transform(days2 = :days .*2,
+                       days3 = :days ./2,
+                       Days = categorical(:days))
 
-fm1 = fit(LinearMixedModel, @formula(Reaction ~ 1 + Days + (1 + Days | Subject)), sleepstudy2)
+# rePCA issue 
+fm1 = fit(LinearMixedModel, @formula(reaction ~ 1 + days + (1 + days | subj)), sleepstudy)
 fm1.rePCA
 
-fm2 = fit(LinearMixedModel, @formula(Reaction ~ 1 + Days2 + (1 + Days2 | Subject)), sleepstudy2)
+fm2 = fit(LinearMixedModel, @formula(reaction ~ 1 + days2 + (1 + days2 | subj)), sleepstudy)
 fm2.rePCA
 
-fm3 = fit(LinearMixedModel, @formula(Reaction ~ 1 + Days3 + (1 + Days3 | Subject)), sleepstudy2)
+fm3 = fit(LinearMixedModel, @formula(reaction ~ 1 + days3 + (1 + days3 | subj)), sleepstudy)
 fm3.rePCA
 
+# fulldummy
+# ... baseline
+fm4 = fit(LinearMixedModel, @formula(reaction ~ 0 + Days + (1 + Days | subj)), sleepstudy)
+
+# ... problem
+fm5 = fit(LinearMixedModel, @formula(reaction ~ 0 + Days + fulldummy(0 + Days | subj)), sleepstudy)
+
+# ... problem
+fm6 = fit(LinearMixedModel, @formula(reaction ~ 0 + Days + fulldummy(1 + Days | subj)), sleepstudy)
 
 
 
