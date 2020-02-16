5f8f3966f9174c8c73965ae53125b0aa8351a81d
Reinhold Kliegl <reinhold.kliegl@gmail.com>
2020-02-16 19:16:07 +0100

    Hot stuff

--- rePCA_demo.jl
+++ rePCA_demo.jl
@@ -1,5 +1,7 @@
-# Problem: rePCA() results not invariant with linear transformation of predictor
-# Reinhold Kliegl, 2020-02-11
+# rePCA() results not invariant with linear transformation of predictor
+# if based on (relative) covariance matrix. This is the new default now.
+# So we are looking at normalized cumulative variance proportions.
+# Reinhold Kliegl, 2020-02-16
 
 using  DataFrames, DataFramesMeta, MixedModels, StatsBase
 
@@ -16,13 +18,23 @@ sleepstudy = @linq sleepstudy |>
                        days3 = :days ./2)
 
 # rePCA issue 
+fm0 = fit(LinearMixedModel, @formula(reaction ~ 1 + days + (1  | Subj)), sleepstudy)
+fm0.rePCA
+
 fm1 = fit(LinearMixedModel, @formula(reaction ~ 1 + days + (1 + days | Subj)), sleepstudy)
 fm1.rePCA
-rePCA(fm1)
-fm2 = fit(LinearMixedModel, @formula(reaction ~ 1 + days2 + (1 + days2 | Subj)), sleepstudy)]
+MixedModels.PCA.(fm1.reterms, corr=true)
+MixedModels.PCA.(fm1.reterms, corr=false)
+
+fm2 = fit(LinearMixedModel, @formula(reaction ~ 1 + days2 + (1 + days2 | Subj)), sleepstudy)
 fm2.rePCA
+MixedModels.PCA.(fm2.reterms, corr=true)
+MixedModels.PCA.(fm2.reterms, corr=false)
 
 fm3 = fit(LinearMixedModel, @formula(reaction ~ 1 + days3 + (1 + days3 | Subj)), sleepstudy)
 fm3.rePCA
+MixedModels.PCA.(fm3.reterms, corr=true)
+MixedModels.PCA.(fm3.reterms, corr=false)
 
-# function rePCA(m::LinearMixedModel, corr::Bool=true; loadings::Bool=false)
+# Another newby: LRT test
+MixedModels.likelihoodratiotest(fm0, fm1)
